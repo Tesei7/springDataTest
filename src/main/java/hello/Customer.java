6ab39775bba73f0485f46690ee3afc73862cc115
Ilya Bochkarev <tesei7@gmail.com>
2017-10-01 17:04:07 +0300

    REST pageable hateoas

--- src/main/java/hello/Customer.java
+++ src/main/java/hello/Customer.java
@@ -3,7 +3,6 @@ package hello;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.joining;
 
--- src/main/java/hello/CustomerController.java
+++ src/main/java/hello/CustomerController.java
@@ -3,13 +3,23 @@ package hello;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.hateoas.PagedResources;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Controller
-@RequestMapping("/users")
+@RequestMapping("/customer")
 public class CustomerController {
 
     @Autowired
@@ -26,5 +36,13 @@ public class CustomerController {
         Page<Customer> customers = repository.findAll(pageable);
         model.addAttribute("customers", customers);
         return "customers";
+    }
+
+    @RequestMapping(value = "/hateoas", method = RequestMethod.GET, produces = {APPLICATION_JSON_VALUE})
+    HttpEntity<PagedResources<Customer>> hateoas(Pageable pageable,
+                                                 PagedResourcesAssembler assembler) {
+
+        Page<Customer> customers = repository.findAll(pageable);
+        return new ResponseEntity<>(assembler.toResource(customers), HttpStatus.OK);
     }
 }
