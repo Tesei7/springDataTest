a691162c367222a2a841cacad82dcb5b71b6615e
Ilia_Bochkarev <tesei7@gmail.com>
2017-10-06 11:12:32 +0300

    @RestResource

--- src/main/java/rest/PersonRepository.java
+++ src/main/java/rest/PersonRepository.java
@@ -1,8 +1,11 @@
 package rest;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
@@ -10,4 +13,7 @@ import java.util.List;
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
 
     List<Person> findByLastName(@Param("name") String name);
+
+    @RestResource(path = "nameStartsWith", rel = "nameStartsWith")
+    Page<Person> findByLastNameStartsWith(@Param("name") String name, Pageable p);
 }
