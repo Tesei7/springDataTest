9346ff2eb11ad01c64ed80c9570e505d3242b9ae
Ilia_Bochkarev <tesei7@gmail.com>
2017-10-11 15:45:14 +0300

    add link

--- src/main/java/rest/RestApplication.java
+++ src/main/java/rest/RestApplication.java
@@ -2,8 +2,12 @@ package rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceProcessor;
 
 @SpringBootApplication
 @Import(RepositoryRestMvcConfiguration.class)
@@ -12,4 +16,17 @@ public class RestApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
     }
+
+    @Bean
+    public ResourceProcessor<Resource<Person>> personProcessor() {
+        return new ResourceProcessor<Resource<Person>>() {
+            @Override
+            public Resource<Person> process(Resource<Person> resource) {
+                resource.add(new Link("http://localhost:8080/people", "added-link"));
+                return resource;
+            }
+        };
+    }
+
+
 }
